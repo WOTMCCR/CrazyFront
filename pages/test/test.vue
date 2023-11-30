67f6debcf4ee5b56724fe3d8e455a9a2bac26ae2
duluge <2237778674@qq.com>
2023-12-01 00:17:34 +0800

    改变颜色的页面

--- pages/test/test.vue
+++ pages/test/test.vue
@@ -1,166 +1,183 @@
 <template>
-	<view>
-		<!-- Navigation options -->
-		<view class="navigation-options">
-			<navigator :style="{ color: currentTab === 0 ? '#ff0000' : '#333333' }" @click="changeTab(0)">
-				<text class="nav-option">Activity</text>
-			</navigator>
-			<navigator :style="{ color: currentTab === 1 ? '#ff0000' : '#333333' }" @click="changeTab(1)">
-				<text class="nav-option">Moment</text>
-			</navigator>
-		</view>
-
-		<view class="group">
-			<image class="image_7" src="../static/back.png" />
-				<view class="header">
-					<image class="avatar" src="../static/head_sculpture.png" />
-					<view class="NameAndDescription">
-						<text class="name">用户昵称</text>
-						<text class="description">我是第一个用户</text>
-					</view>
-				</view>			
-		</view>
-
-		<!-- Swiper for left-right swiping -->
-
-		<!-- Activity List -->
-		<view class="my">
-			<view v-if="List.length > 0" v-for="(item, index) in List" :key="index" @click="gotoDetail(item)">
-				<new-card :detail="item">
-				</new-card>
-			</view>
-			<view v-else>
-				<text>No activity data available.</text>
-			</view>
-		</view>
-	</view>
+  <div class="flex-col section_3" :style="{ background: cardColor }">
+    <div class="flex-row justify-center self-stretch">
+        <div class="relative section_4" @click="changeColor('linear-gradient(180deg, #BEFEE6 0%, #A1E8FF 100%)')"></div>
+        <div class="relative section_5 ml-5" @click="changeColor('linear-gradient(180deg, #A1E8FF 0%, #3897F0 100%)')"></div>
+        <div class="relative section_6 ml-5" @click="changeColor('linear-gradient(180deg, #3897F0 0%, #7B61FF 100%)')"></div>
+        <div class="relative section_7 ml-5" @click="changeColor('linear-gradient(180deg, #FFC0CB 0%, #FFB6C1 100%)')"></div>
+        <div class="relative section_8 ml-5" @click="changeColor('linear-gradient(180deg, #FFD700 0%, #FFA500 100%)')"></div>
+        <div class="section_9 ml-5" @click="changeColor('linear-gradient(180deg, #fd5ba7 0%,  #e85a6d 100%)')"></div>
+    </div>
+    <span class="self-start font text_2">标题</span>
+    <div class="flex-row justify-between items-center section_10 input">
+      <span class="font_2 text_3">标题</span>
+    </div>
+    <span class="self-start font text_4">地点</span>
+    <div class="flex-row justify-between items-center section_10 input">
+      <span class="font_2 text_5">地点</span>
+    </div>
+    <span class="self-start font text_6">联系方式</span>
+    <div class="flex-row justify-between items-center section_10 input">
+      <span class="font_2 text_7">负责人联系方式</span>
+    </div>
+    <span class="self-start font text_8">介绍</span>
+    <div class="flex-col self-stretch group_2">
+      <div class="flex-row justify-between items-center self-stretch input">
+        <span class="font_2">活动内容、主题等</span>
+      </div>
+      <div class="mt-24 flex-col justify-center items-center button text-wrapper" >
+        <span class="text_9">创建</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			currentTab: 0,
-			List: [],
-
-		};
-	},
-	onLoad() {
-		this.loadData();
-	},
-	methods: {
-		async loadData() {
-			try {
-				// Assuming you have a common API endpoint for both Activity and Moment data
-				//"/activity/findByUser/{userId}" :
-				const endpoint = this.currentTab === 0 ? "/activity/findByUserId/1" :
-					"/moment/findByUserId/1";
-				const {
-					statusCode,
-					data: res
-				} = await uni.$http.get(endpoint);
-				console.log(res);
-				console.log(statusCode);
-				if (statusCode == "200") {
-					console.log(this.currentTab);
-					// Update the corresponding list based on the current tab
-					this.List = res;
-					uni.$showMsg("Data loaded successfully");
-				} else {
-					console.error("Failed to load data. Server returned status code:", statusCode);
-				}
-			} catch (error) {
-				console.error("Error fetching data:", error);
-			} finally {
-				uni.hideLoading();
-			}
-		},
-		gotoDetail(item) {
-			uni.navigateTo({
-				url: '/subpkg/testdetail/testdetail?id=' + item.id
-			});
-
-		},
-		changeTab(tabIndex) {
-			this.currentTab = tabIndex;
-			this.loadData();
-		},
-		swiperChange(e) {
-			this.currentTab = e.detail.current;
-			this.loadData();
-		},
-	},
+  data() {
+    return {
+      cardColor: 'linear-gradient(180deg, #f7e0eb 0%, #fed5ad 100%)',
+    };
+  },
+  methods: {
+    changeColor(color) {
+      this.cardColor = color;
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
-.my {
-	padding-left: 40.12rpx;
-	padding-right: 40.12rpx;
+.flex-row {
+  display: flex;
+  flex-direction: row;
 }
 
-.navigation-options {
-	display: flex;
-	justify-content: space-around;
-	margin-top: 10px;
+.flex-col {
+  display: flex;
+  flex-direction: column;
 }
 
-.nav-option {
-	font-size: 16px;
-	color: #333333;
-	text-decoration: underline;
+.justify-center {
+  justify-content: flex-end;
 }
 
-.group {
-	
-.image_7 {
-	width: 100vw;
-	height: 60vw;
+.self-stretch {
+  align-self: stretch;
 }
 
-.header {
-	display: flex;
-	align-items: center;
-	margin-bottom: 20rpx;
-
-	padding: 0 48.84rpx 22.67rpx;
-	background-color: #ffffff;
-	border-radius: 55.81rpx;
-	box-shadow: 0rpx 6.98rpx 6.98rpx #00000040;
-	height: 160rpx;
-
-	margin: -66.28rpx 0 -13.95rpx;
-	.avatar {
-		margin-top: -30.56rpx;
-		border-radius: 83.72rpx;
-		width: 174.42rpx;
-		height: 174.42rpx;
-	}
-
-	.NameAndDescription {
-		display: flex;
-		flex-direction: column;
-		margin-left: 20rpx;
-		margin-top: 20rpx;
-
-		.name {
-			font-size: 27.91rpx;
-			//字体
-			font-family: open;
-			line-height: 21.59rpx;
-			font-weight: 600;
-			color: #000000;
-			margin-bottom: 15rpx;
-		}
-
-		.description {
-			font-size: 27.91rpx;
-			font-family: Inter;
-			line-height: 25.85rpx;
-			color: #536471;
-		}
-
-	}
+.ml-5 {
+  margin-left: 8.78rpx;
 }
+
+.section_3 {
+  margin-top:10px;
+  margin-left: 36.89rpx;
+  margin-right: 42.15rpx;
+  padding: 59.18rpx 28.1rpx;
+  background-image: linear-gradient(180deg, #f7e0eb 0%, #fed5ad 100%);
+  border-radius: 56.21rpx;
+  filter: drop-shadow(0rpx 7.03rpx 10.54rpx #00000026);
+  overflow: hidden;
 }
+
+.section_4,
+.section_5,
+.section_6,
+.section_7,
+.section_8,
+.section_9 {
+  width: 35.13rpx;
+  height: 35.13rpx;
+  border-radius: 175.64rpx;
+  box-shadow: 0rpx 5.27rpx 5.27rpx #00000040;
+}
+
+.section_4 {
+  background-image: linear-gradient(180deg, #BEFEE6 0%, #A1E8FF 100%);
+}
+
+.section_5 {
+  background-image: linear-gradient(180deg, #3897F0 0%, #A1E8FF 100%);
+}
+
+.section_6 {
+  background-image: linear-gradient(180deg, #3897F0 0%, #7B61FF 100%);
+}
+
+.section_7 {
+  background-image: linear-gradient(180deg, #FFC0CB 0%, #FFB6C1 100%); /* 粉红色系 */
+}
+
+.section_8 {
+  background-image: linear-gradient(180deg, #FFD700 0%, #FFA500 100%); /* 黄色/橙色系 */
+}
+
+.section_9 {
+  background-image: linear-gradient(180deg, #fd5ba7 0%,  #e85a6d 100%); /* 绿色系 */
+}
+
+.font {
+  font-size: 28.1rpx;
+  font-family: Inter;
+  line-height: 26.17rpx;
+  color: #0f0f0f;
+}
+
+.text_2,
+.text_4,
+.text_6,
+.text_8 {
+  margin-top: 28.1rpx;
+  line-height: 25.82rpx;
+}
+
+.section_10 {
+  align-self: stretch;
+  margin-top: 17.56rpx;
+}
+
+.input {
+  padding: 24.59rpx 14.05rpx;
+  background-color: #fdeff9;
+  border-radius: 14.05rpx;
+}
+
+.font_2,
+.text_3,
+.text_5,
+.text_7 {
+  line-height: 22.61rpx;
+}
+
+.group_2 {
+  margin-top: 17.56rpx;
+}
+
+.button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+  margin-top: 24.59rpx;
+}
+
+.text-wrapper {
+  padding: 28.1rpx 0;
+  background-color: #ffffff;
+  border-radius: 7.03rpx;
+  filter: drop-shadow(0rpx 8.78rpx 13.17rpx #252c6126, 0rpx 3.51rpx 3.51rpx #8890c233);
+  overflow: hidden;
+  width: 168.62rpx;
+}
+
+.text_9 {
+  color: #545454;
+  font-size: 28.1rpx;
+  font-family: Lato;
+  font-weight: 700;
+  line-height: 26.56rpx;
+}
+
 </style>

4c45a075e7d87637cfd0aa9dde120e7bcd6f5d0f
Jason <2578815178@qq.com>
2023-12-07 18:06:18 +0800

    get data

--- pages/test/test.vue
+++ pages/test/test.vue
@@ -1,28 +1,26 @@
 <template>
-	<view class="mt-10"></view>
-  <view class="flex-col section_3">
+  <view class="flex-col activity-section">
     <view class="flex-row items-center">
       <image
-        class="image_7"
-        :src="imageSrc"
+        class="user-avatar"
+        :src="user.avatar"
       />
-      <view class="flex-col items-start ml-13">
-        <text class="font text_2">{{ username }}</text>
-        <text class="font text_3 mt-7">{{ timestamp }}</text>
+      <view class="flex-col items-start user-info">
+        <text class="user-name">{{ user.name }}</text>
+        <text class="timestamp">{{ timestamp }}</text>
       </view>
     </view>
-    <view class="flex-col items-start group_2 mt-41">
-      <text class="font">标题：{{ title }}</text>
-      <text class="font">活动地址：{{ address }}</text>
-      <text class="font">时间：{{ time }}</text>
-      <text class="font">负责人：{{ organizer }}</text>
-      <text class="font">要求：{{ requirements }}</text>
-      <text class="font">介绍：{{ introduction }}</text>
+    <view class="flex-col items-start activity-details mt-41">
+      <text class="activity-title">标题：{{ title }}</text>
+      <text class="activity-address">活动地址：{{ address }}</text>
+      <text class="activity-time">时间：{{ time }}</text>
+      <text class="activity-organizer">负责人：{{ organizer }}</text>
+      <text class="activity-requirements">要求：{{ requirements }}</text>
+      <text class="activity-introduction">介绍：{{ introduction }}</text>
     </view>
-	<view class="mt-10"></view>
-	<view class="mt-74 flex-col justify-start items-center button text-wrapper">
-	      <text class="text_4">时间线</text>
-	</view>
+    <view class="flex-col justify-start items-center text-wrapper">
+      <text class="timeline-text">时间线</text>
+    </view>
   </view>
 </template>
 
@@ -30,16 +28,58 @@
 export default {
   data() {
     return {
-      imageSrc: "https://ide.code.fun/api/image?token=6569458efcfbac00113616d6&name=c2c19904ed48e72c673d99a1d72c81c5.png",
-      username: "Gavin Nelson",
-      timestamp: "今天 12:00",
-      title: "原神·启动",
-      address: "", // Add the actual address data
-      time: "", // Add the actual time data
-      organizer: "", // Add the actual organizer data
-      requirements: "要玩原神",
-      introduction: "原神交友会已经报名准备参加",
+      user: {
+        name: "",
+        id: "",
+        avatar: ""
+      },
+      timestamp: "",
+      title: "",
+      address: "",
+      time: "",
+      organizer: "",
+      requirements: "",
+      introduction: ""
     };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      var myHeaders = new Headers();
+      myHeaders.append("User-Agent", "Apifox/1.0.0 (https://apifox.com)");
+      myHeaders.append("Accept", "*/*");
+      myHeaders.append("Host", "127.0.0.1:4523");
+      myHeaders.append("Connection", "keep-alive");
+
+      var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+
+      fetch("http://127.0.0.1:4523/m1/3664198-0-default/activity/getnew/1", requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          this.user = data.user;
+          this.timestamp = data.activityTime;
+          this.title = data.name;
+          this.address = data.schedule[0].address;
+          this.time = data.schedule[0].time;
+          this.organizer = data.official;
+          this.requirements = data.participantMax;
+          this.introduction = data.content;
+
+          // 添加成功获取数据的提示
+          uni.$showMsg("数据请求成功");
+        })
+        .catch(error => {
+          console.error("Error fetching data:", error);
+          // 添加失败获取数据的提示
+          uni.$showMsg("数据请求失败，请稍后重试");
+        });
+    },
   },
 };
 </script>
@@ -59,79 +99,32 @@ export default {
     align-items: center;
   }
 
-  .items-start {
-    align-items: flex-start;
-  }
-
-  .ml-13 {
-    margin-left: 22.67rpx;
-  }
-
-  .mt-7 {
-    margin-top: 12.21rpx;
-  }
-
-  .mt-41 {
-    margin-top: 71.51rpx;
-  }
-
-  .section_3 {
-      margin: 0 10.47rpx;
-      padding: 34.88rpx 43.6rpx 104.65rpx;
-      background-image: linear-gradient(180deg, #befee6 0%, #d0f7fb 100%);
-      border-radius: 55.81rpx;
-      filter: drop-shadow(0rpx 6.98rpx 10.47rpx #00000026);
-}
-
-  .image_7 {
+  .user-avatar {
     border-radius: 83.72rpx;
     width: 83.72rpx;
     height: 83.72rpx;
   }
 
-  .font {
-    font-size: 27.91rpx;
-    font-family: Inter;
-    line-height: 41.86rpx;
-    color: #000000;
+  .user-info {
+    margin-left: 22.67rpx;
   }
 
-  .text_2 {
-    font-weight: 600;
-    line-height: 21.59rpx;
-  }
-
-  .text_3 {
+  .timestamp {
     color: #536471;
     line-height: 25.85rpx;
   }
 
-  .group_2 {
+  .activity-section {
+    margin: 20rpx 10.47rpx 0;
+    padding: 34.88rpx 43.6rpx 104.65rpx;
+    background-image: linear-gradient(180deg, #befee6 0%, #d0f7fb 100%);
+    border-radius: 55.81rpx;
+    filter: drop-shadow(0rpx 6.98rpx 10.47rpx #00000026);
+  }
+
+  .activity-details {
     padding: 0 13.95rpx;
-  }
-.mt-10 {
-    margin-top: 30rpx; /* 调整间隔的大小，根据需要调整 */
-  }
-
-  .mt-74 {
-    margin-top: 74rpx;
-  }
-
-  .flex-col {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .justify-start {
-    justify-content: flex-start;
-  }
-
-  .items-center {
-    align-items: center;
-  }
-
-  .button {
-    margin: 0 34.88rpx;
+    margin-top: 71.51rpx;
   }
 
   .text-wrapper {
@@ -140,15 +133,14 @@ export default {
     border-radius: 55.81rpx;
     filter: drop-shadow(0rpx 6.98rpx 10.47rpx #00000026);
     overflow: hidden;
+    margin-top: 20rpx; /* 这里增加了间隔 */
   }
 
-  .text_4 {
+  .timeline-text {
     color: #000000;
     font-size: 45.35rpx;
     font-family: Oswald;
     font-weight: 700;
     line-height: 42.77rpx;
   }
-  
-  /* Add any other styles based on your design */
 </style>
